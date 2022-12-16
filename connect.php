85317e7d3f1a3821ac63c21fde1ec389f6e9fa4b
Kays8 <thirayut1379@gmail.com>
2022-12-16 16:21:02 +0700

    new

--- connect.php
+++ connect.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bussiness</title>
-</head>
-<body>
-    
+
 <?php
 $serverName = 'localhost';
 $userName = 'root';
@@ -27,5 +19,3 @@ try {
 ?>
 
 
-</body>
-</html>
