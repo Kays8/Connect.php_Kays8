87cc8039572205eec80f3b38ad936f7358795f09
Kays8 <thirayut1379@gmail.com>
2023-01-08 20:19:32 +0700

    "

--- business/select0.php
+++ business/select0.php
@@ -1,10 +1,12 @@
 <?php
-require "connect.php";
-// ลองให้โชว์ข้อมูล customer
-$sql = "SELECT * FROM customer WHERE CustomerID = 'Cus005' "; 
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-echo '<br>';
-$result = $stmt->fetchAll();
-print_r($result);
+    require "connect.php";
+    // ลองให้โชว์ข้อมูล customer
+    $sql = "SELECT * FROM customer WHERE CustomerID = 'Cus001' "; 
+
+    // การ prepare คือการทีจะเริ่มประมวลผล
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    echo '<br>';
+    $result = $stmt->fetchAll();
+    print_r($result);
 ?>
